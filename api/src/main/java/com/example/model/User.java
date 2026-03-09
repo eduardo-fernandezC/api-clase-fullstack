7328d076245a6a565f9f17e678bf42a98d729f74
Eduardo Fernandez <ed.fernandezc@duocuc.cl>
2026-03-09 17:12:15 -0300

    User ok

--- api/src/main/java/com/example/model/User.java
+++ api/src/main/java/com/example/model/User.java
@@ -1,5 +1,37 @@
 package com.example.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false, length = 32)
+    private String email;
+
+    @Column(nullable = false, length = 16)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
 }
