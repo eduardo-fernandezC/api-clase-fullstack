fc56c22c0003764c771d1072c7f1f933f0b6e27a
Eduardo Fernandez <ed.fernandezc@duocuc.cl>
2026-03-10 16:54:59 -0300

    Create findAll

--- api/src/main/java/com/example/services/UserService.java
+++ api/src/main/java/com/example/services/UserService.java
@@ -1,5 +1,7 @@
 package com.example.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,10 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
 
     public User save(User user) {
         return userRepository.save(user);
